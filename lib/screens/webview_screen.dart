ed61f58fc1d4fb9ed37e47f57e56d90b559af5be
seongyunLim <lsy96999@gmail.com>
2021-04-21 15:19:27 +0900

    webview clear

--- lib/screens/webview_screen.dart
+++ lib/screens/webview_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -7,6 +10,13 @@ class WebViewScreen extends StatefulWidget {
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if(Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +29,49 @@ class _WebViewScreenState extends State<WebViewScreen> {
           children: [
             Expanded(
                 child: WebView(
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (controller) {
+                    _controller.complete(controller);
+                  },
                   initialUrl: 'https://flutter.dev',
                 )
             ),
+            FutureBuilder(
+              future: _controller.future,
+              builder: (context,AsyncSnapshot<WebViewController> snapshot) {
+                final webViewReady = snapshot.connectionState == ConnectionState.done;
+                final controller = snapshot.data;
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        icon: Icon(Icons.arrow_forward_ios),
+                        onPressed: webViewReady ? () async{
+                          if(await controller.canGoForward()){
+                            await controller.goForward();
+                          } else {
+                            print('history 없음');
+                          }
+                        }: null
+                        ),
+                    IconButton(icon: Icon(
+                        Icons.refresh),
+                        onPressed: webViewReady ? (){
+                          controller.reload();
+                        } : null,
+                    ),
+                    IconButton(
+                        icon: Icon(Icons.arrow_back_ios),
+                        onPressed:webViewReady ? () async{
+                          if(await controller.canGoBack()){
+                            await controller.goBack();
+                          }
+                        }: null
+                        ),
+                  ],
+                );
+              },
+            )
           ],
         ),
       ),
