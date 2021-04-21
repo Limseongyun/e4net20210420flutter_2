0c903e8d269382ecd290267a24406f7cb86404c4
seongyunLim <lsy96999@gmail.com>
2021-04-21 10:19:55 +0900

    first commit

---
+++ android/app/src/main/java/net/e4net/flutter_e4net_2/MainActivity.java
@@ -0,0 +1,15 @@
+package net.e4net.flutter_e4net_2;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterFragmentActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
